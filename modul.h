4049b725996b6948514fa610b5d0163035cc1b49
Michał <michalsuski02@gmail.com>
2022-05-26 03:05:37 +0200

    naglowek

---
+++ modul.h
@@ -0,0 +1,13 @@
+//
+// Created by ADMIN on 26.05.2022.
+//
+
+#ifndef AZIK6_2_MODUL_H
+#define AZIK6_2_MODUL_H
+
+
+double mnoz(double i,double j);
+double dziel(double i,double j);
+double dodaj(double i,double j);
+double odejmij(double  i,double j);
+#endif //AZIK6_2_MODUL_H
