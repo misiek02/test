272d5491a03add1d5947f51c32d213a4892e7fd2
Michał <michalsuski02@gmail.com>
2022-05-26 02:35:29 +0200

    plik main

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+double dodawanie(double i, double j)
+{
+    double Wynik;
+    Wynik = i + j;
+
+    return Wynik;
+}
+
+int main()
+{
+
+    cout << dodawanie(4,5) << endl;
+
+}
