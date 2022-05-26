e4f4197f2a346404aa804e8616be228c7eddec6a
Michał <michalsuski02@gmail.com>
2022-05-26 03:06:11 +0200

    nmodul

--- main.cpp
+++ main.cpp
@@ -1,18 +1,40 @@
 #include <iostream>
+#include "modul.h"
 using namespace std;
-
-
-double dodawanie(double i, double j)
-{
-    double Wynik;
-    Wynik = i + j;
-
-    return Wynik;
-}
 
 int main()
 {
+    char z=0;
+    auto a=0,b=0;
 
-    cout << dodawanie(4,5) << endl;
+    cout << "Podaj 2 liczby do dzialania:" << endl;
+    cout << "============================" << endl;
+    cin >> a >> b;
+    cout << endl << "Podaj znak operacji" << endl;
+    cin >> z;
+    cout << endl;
+
+        switch (z) {
+            case '+': {
+                cout << "Wynik operacji:  " << dodaj(a, b) << endl;
+            }
+                break;
+            case '-': {
+                cout << "Wynik operacji:  " << odejmij(a, b) << endl;
+            }
+                break;
+            case '*': {
+                cout << "Wynik operacji:  " << mnoz(a, b) << endl;
+            }
+                break;
+            case '/': {
+                cout << "Wynik operacji:  " << dziel(a, b) << endl;
+            }
+                break;
+            default:
+                cout << "Nie ma takiej operacji" << endl;
+    }
+    return 0;
+
 
 }
---
+++ modul.cpp
@@ -0,0 +1,31 @@
+//
+// Created by ADMIN on 26.05.2022.
+//
+
+#include "modul.h"
+
+using namespace std;
+
+double mnoz(double i, double j)
+{
+    auto Wynik = i*j;
+    return Wynik;
+}
+
+double dziel(double i, double j)
+{
+    auto Wynik = i/j;
+    return Wynik;
+}
+
+double dodaj(double i, double j)
+{
+    auto Wynik = i+j;
+    return Wynik;
+}
+
+double odejmij(double i, double j)
+{
+    auto Wynik = i-j;
+    return Wynik;
+}
